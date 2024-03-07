1d94f3a9d3e1c72dcf6d375c7f19f356f78e9ae8
zobayerdev <zobayer.dev@gmail.com>
2024-03-07 15:37:51 +0600

    project upload

---
+++ android/app/src/main/kotlin/com/example/convex_bottom_bar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.convex_bottom_bar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/bottom_nav.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class bottom_nav extends StatelessWidget {
+  const bottom_nav({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
