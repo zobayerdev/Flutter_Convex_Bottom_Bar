53f60a5d9f989f1f98289af751748ae2f171bfc1
zobayerdev <zobayer.dev@gmail.com>
2024-03-08 16:07:44 +0600

    project upload

--- lib/bottom_bar.dart
+++ lib/bottom_bar.dart
@@ -1,4 +1,4 @@
-//import 'package:convex_bottom_bar/convex_bottom_bar.dart';
+import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 import 'package:flutter/material.dart';
 
 class Bottom_Bar extends StatelessWidget {
